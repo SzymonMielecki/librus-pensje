c4f79e3a8a0a7a101634587ddc30489e43fa5a00
SzymonMielecki <szymon.mielecki@gmail.com>
2023-03-23 09:39:00 +0100

    testing teacher view

--- src/routes/teacher/[teacherName]/+page.server.ts
+++ src/routes/teacher/[teacherName]/+page.server.ts
@@ -5,46 +5,6 @@ export const load: PageServerLoad = async ({ params }) => {
 	const thisTeacherName: string = params.teacherName;
 	console.log(params);
 	return {
-		contracts: await prisma.contracts.findMany({ where: { teacherName: thisTeacherName } }),
-		september: await prisma.contracts.findMany({
-			where: { teacherName: thisTeacherName, month: 9 },
-			select: { hoursWorked: true }
-		}),
-		october: await prisma.contracts.findMany({
-			where: { teacherName: thisTeacherName, month: 10 },
-			select: { hoursWorked: true }
-		}),
-		november: await prisma.contracts.findMany({
-			where: { teacherName: thisTeacherName, month: 11 },
-			select: { hoursWorked: true }
-		}),
-		december: await prisma.contracts.findMany({
-			where: { teacherName: thisTeacherName, month: 12 },
-			select: { hoursWorked: true }
-		}),
-		january: await prisma.contracts.findMany({
-			where: { teacherName: thisTeacherName, month: 1 },
-			select: { hoursWorked: true }
-		}),
-		february: await prisma.contracts.findMany({
-			where: { teacherName: thisTeacherName, month: 2 },
-			select: { hoursWorked: true }
-		}),
-		march: await prisma.contracts.findMany({
-			where: { teacherName: thisTeacherName, month: 3 },
-			select: { hoursWorked: true }
-		}),
-		april: await prisma.contracts.findMany({
-			where: { teacherName: thisTeacherName, month: 4 },
-			select: { hoursWorked: true }
-		}),
-		may: await prisma.contracts.findMany({
-			where: { teacherName: thisTeacherName, month: 5 },
-			select: { hoursWorked: true }
-		}),
-		june: await prisma.contracts.findMany({
-			where: { teacherName: thisTeacherName, month: 6 },
-			select: { hoursWorked: true }
-		})
+		contracts: await prisma.contracts.findMany({ where: { teacherName: thisTeacherName } })
 	};
 };
--- src/routes/teacher/[teacherName]/+page.svelte
+++ src/routes/teacher/[teacherName]/+page.svelte
@@ -2,7 +2,8 @@
     import { page } from '$app/stores'
     import type { PageData } from './$types';
 	export let data: PageData;
-    $: ({ contracts, october, september, november, december, january, february, march, april, may, june } = data);
+    $: ({ contracts} = data);
+    $: console.log(contracts)
 
     export let teacherName = $page.params.teacherName;
 </script>
@@ -29,157 +30,18 @@
                 </tr>
             </thead>
             <tbody>
-                {#each contracts as contract, i}
+                <!-- {#each contracts as contract, i}
                 <tr>
                     <td>Ilość godzin</td>
                     <td>{contract.subjectName}</td>
                     <td></td>
-                    <td>
-                        {#if september[i] === undefined}
-                            0
-                        {:else}
-                            {Number(september[i].hoursWorked)}
-                        {/if}
-                    </td>
-                    <td>
-                        {#if october[i] === undefined}
-                            0
-                        {:else}
-                            {Number(october[i].hoursWorked)}
-                        {/if}
-                    </td>
-                    <td>
-                        {#if november[i] === undefined}
-                            0
-                        {:else}
-                            {Number(november[i].hoursWorked)}
-                        {/if}
-                    </td>
-                    <td>
-                        {#if december[i] === undefined}
-                            0
-                        {:else}
-                            {Number(december[i].hoursWorked)}
-                        {/if}
-                    </td>
-                    <td>
-                        {#if january[i] === undefined}
-                            0
-                        {:else}
-                            {Number(january[i].hoursWorked)}
-                        {/if}
-                    </td>
-                    <td>
-                        {#if february[i] === undefined}
-                            0
-                        {:else}
-                            {Number(february[i].hoursWorked)}
-                        {/if}
-                    </td>
-                    <td>
-                        {#if march[i] === undefined}
-                            0
-                        {:else}
-                            {Number(march[i].hoursWorked)}
-                        {/if}
-                    </td>
-                    <td>
-                        {#if april[i] === undefined}
-                            0
-                        {:else}
-                            {Number(april[i].hoursWorked)}
-                        {/if}
-                    </td>
-                    <td>
-                        {#if may[i] === undefined}
-                            0
-                        {:else}
-                            {Number(may[i].hoursWorked)}
-                        {/if}
-                    </td>
-                    <td>
-                        {#if june[i] === undefined}
-                            0
-                        {:else}
-                            {Number(june[i].hoursWorked)}
-                        {/if}
-                    </td>
                 </tr>
                 <tr>
                     <td>Wynagrodzenie</td>
                     <td>{contract.subjectName}</td>
                     <td></td>
-                    <td>
-                        {#if september[i] === undefined}
-                            0
-                        {:else}
-                            {Number(september[i].hoursWorked)*Number(contract.hourlyRate)}
-                        {/if}
-                    </td>
-                    <td>
-                        {#if october[i] === undefined}
-                            0
-                        {:else}
-                            {Number(october[i].hoursWorked)*Number(contract.hourlyRate)}
-                        {/if}
-                    </td>
-                    <td>
-                        {#if november[i] === undefined}
-                            0
-                        {:else}
-                            {Number(november[i].hoursWorked)*Number(contract.hourlyRate)}
-                        {/if}
-                    </td>
-                    <td>
-                        {#if december[i] === undefined}
-                            0
-                        {:else}
-                            {Number(december[i].hoursWorked)*Number(contract.hourlyRate)}
-                        {/if}
-                    </td>
-                    <td>
-                        {#if january[i] === undefined}
-                            0
-                        {:else}
-                            {Number(january[i].hoursWorked)*Number(contract.hourlyRate)}
-                        {/if}
-                    </td>
-                    <td>
-                        {#if february[i] === undefined}
-                            0
-                        {:else}
-                            {Number(february[i].hoursWorked)*Number(contract.hourlyRate)}
-                        {/if}
-                    </td>
-                    <td>
-                        {#if march[i] === undefined}
-                            0
-                        {:else}
-                            {Number(march[i].hoursWorked)*Number(contract.hourlyRate)}
-                        {/if}
-                    </td>
-                    <td>
-                        {#if april[i] === undefined}
-                            0
-                        {:else}
-                            {Number(april[i].hoursWorked)*Number(contract.hourlyRate)}
-                        {/if}
-                    </td>
-                    <td>
-                        {#if may[i] === undefined}
-                            0
-                        {:else}
-                            {Number(may[i].hoursWorked)*Number(contract.hourlyRate)}
-                        {/if}
-                    </td>
-                    <td>
-                        {#if june[i] === undefined}
-                            0
-                        {:else}
-                            {Number(june[i].hoursWorked)*Number(contract.hourlyRate)}
-                        {/if}
                 </tr>
-                {/each}
+                {/each} -->
             </tbody>
         </table>
     </div>
