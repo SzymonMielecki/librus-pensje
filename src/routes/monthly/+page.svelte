8c083a1ad44e238c264294800324593e7a2be1be
SzymonMielecki <szymon.mielecki@gmail.com>
2023-03-24 10:47:02 +0100

    maybe fixed the monthly now

--- src/routes/monthly/+page.svelte
+++ src/routes/monthly/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	export let data: PageData;
 	import { Trash } from 'lucide-svelte';
+	export let data: PageData;
 	let selectedMonth: string;
 	$: ({ contracts } = data);
 </script>
