3bf7b82025524e5074bdc56f6ff7ac6d6331ffa4
SzymonMielecki <58358829+SzymonMielecki@users.noreply.github.com>
2023-05-24 09:44:10 +0200

    now working with supabase

--- src/routes/contract/[id]/+page.server.ts
+++ src/routes/contract/[id]/+page.server.ts
@@ -1,7 +1,6 @@
-import type { PageServerLoad } from './$types';
-
 import { prisma } from '$lib/server/prisma';
-export const load: PageServerLoad = async ({ params }) => {
+
+export const load = async ({ params }) => {
 	const thisContractId: string = params.id;
 	const thisContract = await prisma.contract.findUnique({
 		where: {
